3c1c829ba6def4f94cea6d60dfd997a0d997facf
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-16 10:21:43 -0400

    Add solution to exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
     return 1;
 };
 
+function totalLikes(blogs) {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 };
