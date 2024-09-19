6c4ede48851b240d9eceba115f681b820e6920ae
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-19 07:12:54 -0400

    Fix solution to exercise 4.8

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,15 +1,34 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
-const app = require("../app");
 
+const helper = require("./test_helper");
+const app = require("../app");
 const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    for (const blog of helper.initialBlogs) {
+        const blogObject = new Blog(blog);
+        await blogObject.save();
+    }
+});
 
 test("blogs are returned as json", async () => {
     await api
         .get("/api/blogs")
         .expect(200)
         .expect("Content-Type", /application\/json/);
+});
+
+test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 after(async () => {
